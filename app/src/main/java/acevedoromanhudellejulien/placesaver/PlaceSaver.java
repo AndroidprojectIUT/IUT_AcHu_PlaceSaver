edf428e8d6283538f1cf449a8ede2947a842c11e
AcevedoR <roman.acevedo62@gmail.com>
2016-06-03 11:18:18 +0200

    Creating main menu

--- app/src/main/java/acevedoromanhudellejulien/placesaver/PlaceSaver.java
+++ app/src/main/java/acevedoromanhudellejulien/placesaver/PlaceSaver.java
@@ -16,16 +16,11 @@ public class PlaceSaver extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_saver);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Place Saver");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
     }
 
     @Override
